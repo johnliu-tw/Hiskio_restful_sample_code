a6eda23703f3bd79de344c7de4ccf0ce09383e5d
johnliu <johnliutw@hotmail.com>
2019-09-15 20:47:53 +0800

    prettier

--- resources/user.py
+++ resources/user.py
@@ -11,106 +11,103 @@ DB_SCHEMA = "flask_demo"
 
 
 parser = reqparse.RequestParser()
-parser.add_argument('name')
-parser.add_argument('gender')
-parser.add_argument('birth')
-parser.add_argument('note')
+parser.add_argument("name")
+parser.add_argument("gender")
+parser.add_argument("birth")
+parser.add_argument("note")
 
-response = {
-    'code': 200,
-    'msg': 'success'
-}
+response = {"code": 200, "msg": "success"}
 
-class User (Resource):
 
+class User(Resource):
     def db_init(self):
-        db = pymysql.connect(DB_HOST,DB_USER,DB_PASSWORD,DB_SCHEMA)
+        db = pymysql.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_SCHEMA)
         cursor = db.cursor(pymysql.cursors.DictCursor)
-        return db, cursor 
+        return db, cursor
 
-    def get(self, id):    
+    def get(self, id):
         db, cursor = self.db_init()
-        sql = '''Select * FROM flask_demo.users Where id = {}'''.format(id)
+        sql = """Select * FROM flask_demo.users Where id = {}""".format(id)
         cursor.execute(sql)
         db.commit()
         user = cursor.fetchall()
         db.close()
-        response['data'] = user
+        response["data"] = user
         return response
 
     def put(self, id):
         db, cursor = self.db_init()
         arg = parser.parse_args()
         user = {
-            'name': arg['name'],
-            'gender': arg['gender'],
-            'birth': arg['birth'],
-            'note': arg['note']
+            "name": arg["name"],
+            "gender": arg["gender"],
+            "birth": arg["birth"],
+            "note": arg["note"],
         }
         query = []
         for key, value in user.items():
             print(key)
             print(value)
             if value != None:
-                query.append(key + ' = ' + ' \'{}\' '.format(value))
-        query = ','.join(query)
-        
+                query.append(key + " = " + " '{}' ".format(value))
+        query = ",".join(query)
 
-        sql = '''Update flask_demo.users Set {} Where id = {}'''.format(query, id)
+        sql = """Update flask_demo.users Set {} Where id = {}""".format(query, id)
         result = cursor.execute(sql)
         db.commit()
         db.close()
 
-        response['result'] = True if result == 1 else False
+        response["result"] = True if result == 1 else False
 
         return response
 
     def delete(self, id):
         db, cursor = self.db_init()
-        sql = '''Delete FROM flask_demo.users Where id = {}'''.format(id)
+        sql = """Delete FROM flask_demo.users Where id = {}""".format(id)
         result = cursor.execute(sql)
         db.commit()
         db.close()
 
-        response['result'] = True if result == 1 else False
+        response["result"] = True if result == 1 else False
         return response
 
-class Users (Resource):
 
+class Users(Resource):
     def db_init(self):
-        db = pymysql.connect(DB_HOST,DB_USER,DB_PASSWORD,DB_SCHEMA)
+        db = pymysql.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_SCHEMA)
         cursor = db.cursor(pymysql.cursors.DictCursor)
-        return db, cursor        
+        return db, cursor
 
-    def get(self): 
+    def get(self):
         db, cursor = self.db_init()
-        sql = '''Select * FROM flask_demo.users '''
+        sql = """Select * FROM flask_demo.users """
         cursor.execute(sql)
         db.commit()
         users = cursor.fetchall()
         db.close()
         for user in users:
-            user['birth'] = user['birth'].strftime("%Y-%m-%d")
+            user["birth"] = user["birth"].strftime("%Y-%m-%d")
 
-        response['data'] = users
+        response["data"] = users
         return response
 
-    def post(self): 
+    def post(self):
         db, cursor = self.db_init()
         arg = parser.parse_args()
         user = {
-            'name': arg['name'],
-            'gender': arg['gender'],
-            'birth': arg['birth'] or '1900-01-01',
-            'note': arg['note']
+            "name": arg["name"],
+            "gender": arg["gender"],
+            "birth": arg["birth"] or "1900-01-01",
+            "note": arg["note"],
         }
-        sql = '''Insert into flask_demo.users 
+        sql = """Insert into flask_demo.users 
                 (name, gender, birth, note) 
-                values('{}', '{}', '{}', '{}')'''.format(user['name'], user['gender'], user['birth'], user['note'])
+                values('{}', '{}', '{}', '{}')""".format(
+            user["name"], user["gender"], user["birth"], user["note"]
+        )
         result = cursor.execute(sql)
         db.commit()
         db.close()
-        response['result'] = True if result == 1 else False
+        response["result"] = True if result == 1 else False
 
         return response
-        
